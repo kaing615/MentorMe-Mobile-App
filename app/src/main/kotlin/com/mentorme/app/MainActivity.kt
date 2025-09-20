af378d17d9d28af538e2a2a86cf9996f8ac8ef82
Dai Thang <23521422@gm.uit.edu.vn>
2025-09-20 23:19:44 +0700

    fix: blur in GlassCard

--- app/src/main/kotlin/com/mentorme/app/MainActivity.kt
+++ app/src/main/kotlin/com/mentorme/app/MainActivity.kt
@@ -1,16 +1,19 @@
 // MainActivity.kt
 package com.mentorme.app
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.SystemBarStyle
+import androidx.annotation.RequiresApi
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import com.mentorme.app.ui.navigation.AppNav
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         // Trong suốt hoàn toàn cho status + navigation bars
         enableEdgeToEdge(
